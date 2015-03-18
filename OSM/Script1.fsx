9897c22eff63243815db196c3da566c2dddd611d
archaeron <whatamaps@gmail.com>
2015-03-18 20:11:49 +0100

    init

---
+++ OSM/Script1.fsx
@@ -0,0 +1,17 @@
+﻿#I @"..\packages\FSharp.Data.2.1.1\lib\net40"
+
+#r "System.Xml.Linq.dll"
+#r "FSharp.Data.dll"
+
+open FSharp.Data
+
+let file = @".\Data\map.osm"
+
+type Map = XmlProvider<".\Data\map.osm">
+
+let map = Map.Load(file)
+
+for node in map.Nodes do
+    if node.Tags.Length > 0 then
+        printfn "%A" node.Tags
+    
